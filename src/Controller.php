670cf2d61e19b51600e9fd996a041faf4e41759d
Raffael Kessler <raffael.kessler@prowect.com>
2016-05-28 15:42:03 +0200

    MinimizeCompiler hinzugefügt

--- src/Controller.php
+++ src/Controller.php
@@ -3,17 +3,23 @@
 namespace Drips\LessCompiler;
 
 use Drips\MVC\CompileController;
+use Drips\MinimizeCompiler\MinimizeCompiler;
 
 class Controller extends CompileController
 {
     protected $source_directory = "less";
     protected $file_extension = "less";
     protected $response_type = "text/css";
+    protected $minimize = true;
 
     public function compile($content)
     {
         $compiler = new LessCompiler;
         $compiler->setImportDir($this->source_directory);
-        return $compiler->compile($content);
+        $compiled = $compiler->compile($content);
+        if($this->minimize){
+            $compiled = MinimizeCompiler::compile($compiled, MinimizeCompiler::CSS);
+        }
+        return $compiled;
     }
 }
